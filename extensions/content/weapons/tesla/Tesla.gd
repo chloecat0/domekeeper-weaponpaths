b62a02bb8a5009c61aee87325c3c756b15cb1630
chloecat0 <43815120+TheChaser212@users.noreply.github.com>
2025-02-24 22:08:46 +1300

    chain lightning functions

--- extensions/content/weapons/tesla/Tesla.gd
+++ extensions/content/weapons/tesla/Tesla.gd
@@ -1,30 +1,98 @@
 extends "res://content/weapons/tesla/Tesla.gd"
 
+@onready
+var chainArea = $ChainArea2D
+@onready
+var chainCollisionShape = $ChainArea2D/CollisionShape2D
+@onready
+var chainArcs = [$ChainArea2D/ShootChain1, $ChainArea2D/ShootChain2,
+	$ChainArea2D/ShootChain3, $ChainArea2D/ShootChain4,
+	$ChainArea2D/ShootChain5, $ChainArea2D/ShootChain6]
+
 func shootSingle(perfectTiming:bool, quickshot:bool):
 	super(perfectTiming, quickshot)
-	var chains = Data.of("tesla.chains")
+	var chains = Data.ofOr("tesla.chains", 0)
+	print(chains)
 	if(chains <= 0):
 		return
-	var chainDistance = Data.of("tesla.chaindistance")
+	var chainDistance = Data.of("tesla.chaindistance")*10
 	var splits = Data.of("tesla.splits")
 	
-	var reticleStart = reticle.global_position
-	var reticleAutoAimVisible = reticle.autoAimArea.visible
-	var reticleCollision = $reticle/AutoAimArea/CollisionShape2D
-	var reticleStartRadius = reticleCollision.shape.radius
-	var splitPosition = reticle.global_position
-	
-	reticle.autoAimArea.shape.radius *= chainDistance
-	reticle.autoAimArea.visible = true
+	print("Chains: ",chains," Chain Distance: ",chainDistance," Splits: ",splits)
+	#var lastPosition = reticle.position
+	var nextPosition = reticle.position
 	for chain in range(chains):
-		splitPosition = reticle.global_position
-		for split in range(splits):
-			reticle.global_position = splitPosition
-			var monster = reticle.getAutoaimTarget()
-			if(monster): #monster nearby
-				reticle.global_position = monster.global_position
-				super(false, false)
-		
-	reticleCollision.shape.radius = reticleStartRadius
-	reticle.autoAimArea.visible = reticleAutoAimVisible
-	reticle.global_position = reticleStart
+		#lastPosition = chainArea.position
+		var monsters = getChainLightningTargets()
+		print(monsters)
+		for split in range(min(splits, monsters.size())):
+			print("Chain",chain," Split",split)
+			print("firing chain")
+			shootChain(chainArea.position, monsters[split].global_position, chain*splits + split)
+			nextPosition = monsters[split].global_position
+		chainArea.position = nextPosition
+
+
+func getChainLightningTargets() -> Array:
+	chainCollisionShape.shape.radius = 50*Data.of("tesla.chaindistance")
+	var monsters : Array = chainArea.get_overlapping_areas()
+	if monsters.is_empty():
+		return []
+	var hittableMonsters := []
+	for m in monsters:
+		if m.canBeHit():
+			hittableMonsters.append(m)
+	hittableMonsters.sort_custom(Callable(self, "sortByDistanceChain"))
+	hittableMonsters.reverse()
+	if hittableMonsters.is_empty():
+		return []
+	return hittableMonsters
+
+func shootChain(startPosition:Vector2, targetPosition:Vector2, arcIndex:int):
+	var powervfx = Data.of("tesla.powervfx")
+	var shotPosition = targetPosition
+	var newImpactSound = preload("res://content/weapons/tesla/sounds/TeslaShotSound.tscn").instantiate()
+	Level.viewports.addElement(newImpactSound)
+	newImpactSound.play(powervfx, shotPosition, false)
+	
+	var newImpact = preload("res://content/weapons/tesla/TeslaImpact.tscn").instantiate()
+	newImpact.position = shotPosition
+	newImpact.setPower(powervfx)
+	Level.map.addPixelEffect(newImpact)
+	
+	chainArcs[arcIndex].position = startPosition
+	chainArcs[arcIndex].target = shotPosition
+	var arcDuration = remap(float(powervfx), 0.0, 4.0, 0.7, 2.0)
+	chainArcs[arcIndex].forkSpawnArea = Vector2(4,4) * arcDuration
+	$Tween.interpolate_method(self, "controlChainArc", [arcIndex, 1], 0, arcDuration, Tween.TRANS_SINE, Tween.EASE_OUT)
+	
+	var newShot = preload("res://content/weapons/tesla/TeslaThunder.tscn").instantiate()
+	newShot.perfect = false
+	newShot.global_position = shotPosition
+	
+	var radius = Data.of("tesla.radius")
+	var baseDamage = Data.of("tesla.damage")
+	var damage = baseDamage
+	var strength = Data.of("tesla.persistance")
+	
+	$Tween.start()
+	shotCharge = 0
+	reloadedSoundPlayed = false
+	
+	reticle.shoot()
+	
+	Level.viewports.addElement(newShot)
+	newShot.start(damage, radius, strength)
+
+func sortByDistanceChain(a, b):
+	return a.global_position.distance_to(global_position) > b.global_position.distance_to(global_position)
+
+func controlChainArc(index:int, v:float):
+	chainArcs[index].visible = v >= 0.05
+	#chainArc.position = (Vector2.RIGHT*float(Data.of("tesla.powervfx"))*2.0).rotated(randf()*TAU)
+	var dir = sign(chainArea.global_position.x - chainArcs[index].target.x)
+	chainArcs[index].baseCurvature = remap(v, 1.0, 0.0, 0.0, 0.8) * dir
+	chainArcs[index].spawnInterval = remap(ease(v, 0.5), 1.0, 0.0, 0.01, 0.2)
+	chainArcs[index].arcWildness = remap(v, 1.0, 0.0, 0.0, 20.0)
+	chainArcs[index].modulate = lerp(Color(2,2,2,1), Color(1,1,1,1), ease(v, 1.2))
+	chainArcs[index].impactRadius = remap(ease(v, 0.3), 1.0, 0.0, 4.0, 30.0)
--- overwrites.gd
+++ overwrites.gd
@@ -4,6 +4,9 @@ const MOD_PATH := "res://mods-unpacked/chloecat-weaponpaths/"
 const MOD_ICON_PATH := MOD_PATH + "overwrites/content/icons/"
 const GAME_ICON_PATH := "res://content/icons/"
 
+const OVERWRITE_PATH := MOD_PATH + "overwrites/"
+const TESLA_SCENE := "content/weapons/tesla/Tesla.tscn"
+
 var icons := [
 	"teslachain.png",
 	"teslachaindistance1.png","teslachaindistance2.png","teslachaindistance3.png",
@@ -18,5 +21,9 @@ func _init():
 		var overwrite = load(MOD_ICON_PATH+icon)
 		iconTextures.append(overwrite)
 		overwrite.take_over_path(GAME_ICON_PATH+icon)
-		
+	
+	var scene_overwrite = preload(OVERWRITE_PATH+TESLA_SCENE)
+	loaded.append(scene_overwrite)
+	scene_overwrite.take_over_path("res://"+TESLA_SCENE)
+	
 	ModLoaderLog.info("Completed overwrites", "chloecat-weaponpaths")
